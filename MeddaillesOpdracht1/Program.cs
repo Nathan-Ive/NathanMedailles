ccc30e0a46c270f36cf1765baa64044523a90d21
Marrow-SG <2242205@talnet.nl>
2025-11-12 10:51:46 +0100

    The completion of the first task
    I wrote code that asks for you to input your name, returns your name, asks for your age, and denies your input if you put in anything other than a number.

---
+++ MeddaillesOpdracht1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeddaillesOpdracht1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello user, what is your name?");
+            String naamInput = Console.ReadLine();
+            String gebruikerNaam = naamInput;
+            Console.WriteLine("Welcome " + gebruikerNaam + " can you fill in your age next?");
+            String ageInput = Console.ReadLine();
+            int gebruikerAge;
+            Boolean ageNumberCheck;
+            if (!Int32.TryParse(ageInput, out gebruikerAge)) {
+                Console.WriteLine(gebruikerNaam + ", please try and input an age exclusively using numbers. Thank you.");
+            }
+            else { Console.WriteLine("Hello, " + gebruikerNaam + ". Your age is " + gebruikerAge); }
+        }
+    }
+}
