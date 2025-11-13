f8368a279dd57d8ed4773eced723a3ec7dbbfbd2
Marrow-SG <2242205@talnet.nl>
2025-11-13 11:06:20 +0100

    Task 1 transfer
    I am pushing this separately from the other files because I finished one, and copied it over to this folder, basically needed a small explanation for that. To see the actual process for task 1, you'll have to check the task 1 folder. It should still exist for the sake of checking the initial development of that task.

---
+++ MeddaillesOpdracht2/Opdracht1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeddaillesOpdrachten
+{
+    internal class Opdracht1
+    {
+        public void Start()
+        {
+            String naamInput;
+            String gebruikerNaam;
+            String ageInput;
+            int gebruikerAge;
+
+
+
+            Console.WriteLine("Hello user, what is your name?");
+            naamInput = Console.ReadLine();
+            gebruikerNaam = naamInput;
+
+            Console.WriteLine("Welcome " + gebruikerNaam + " can you fill in your age next?");
+            ageInput = Console.ReadLine();
+
+            if (!Int32.TryParse(ageInput, out gebruikerAge))
+            {
+                Console.WriteLine(gebruikerNaam + ", please try and input an age exclusively using numbers. Thank you.");
+            }
+            else { Console.WriteLine("Hello, " + gebruikerNaam + ". Your age is " + gebruikerAge); }
+        }
+    }
+}
