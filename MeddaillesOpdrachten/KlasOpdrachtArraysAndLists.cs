b5eb1cedf00b14a73d8c5acdb3df4e3c86a49ca3
Marrow-SG <2242205@talnet.nl>
2025-11-24 12:35:39 +0100

    Klas Medals
    These aren't actual tasks related to the ones I have to turn in. These are tasks given during class that are saved here for the sake of logging everything I do in the class. It's easier to simply add them to the relevant term's set of tasks than to make a unique repository for it, so here it is.

---
+++ MeddaillesOpdrachten/KlasOpdrachtArraysAndLists.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeddaillesOpdracht2
+{
+    internal class KlasOpdrachtArraysAndLists
+    {
+        //maak 3 lists
+        ///string
+        ///int
+        ///boolean
+        //gebruik voor alle 3 de add functie
+        //gebruik alle 3 de functies de je had opgeschreven vanuit de vorige opdracht
+        //zoek op hoe je alle lists helemaal zou kunnen printen
+
+        private List<string> theWords = new List<string>();
+        private List<int> theNumbers = new List<int>();
+        private List<Boolean> theTruth = new List<Boolean>();
+
+
+        public void Start()
+        {
+            theWords.Add("This is how you use a list!");
+            theWords.Add("You can just keep adding like this. There's also the AddRange command, but...");
+            theWords.Add("I don't know how to use that.");
+            theNumbers.Add(3);
+            theNumbers.Add(15);
+            theNumbers.Add(24);
+            theTruth.Add(true);
+            theTruth.Add(false);
+            theTruth.Add(false);
+
+
+            Console.WriteLine(theWords[1]);
+            Console.WriteLine(theNumbers[1]);
+            Console.WriteLine(theTruth[0]);
+
+
+        }
+
+    }
+}
---
+++ MeddaillesOpdrachten/KlasOpdrachtFuncties.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Remoting.Lifetime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeddaillesOpdracht2
+{
+    internal class KlasOpdrachtFuncties
+    {
+        //Access modifier
+        //1. Maak een functie die een boolean returned. Als parameter geef je een getal mee, In de functie check je of het getal groter is dan 0. zo ja dan return je true, zo nee return je false
+        //2. maak een functie die 2 getallen bij elkaar optelt en deze getallen returned
+        //3. maak een functie die ":)" achter je tekst (string) plakt en return deze tekst
+        // print alle 3 de werkende functie resultaten uit.
+
+        private Boolean validity;
+        private int numbers;
+        private String words;
+
+
+
+
+        private Boolean PositivityCheck(int coolNumber) 
+        {
+            return coolNumber > 0;
+        }
+
+        private int NumberFuser(int firstNumber, int secondNumber) 
+        {
+            int theCombined = firstNumber + secondNumber;
+            return theCombined;
+        }
+
+        private String SmileyMaker(String userText) 
+        {
+            String theSmile = userText + " :) ";
+
+            return theSmile;
+        }
+
+
+        public void Start()
+        {
+            validity = PositivityCheck(1);
+            numbers = NumberFuser(2, 5);
+            words = SmileyMaker("This is my text.");
+
+            Console.WriteLine(validity);
+            Console.WriteLine(numbers);
+            Console.WriteLine(words);
+        }
+
+        
+    }
+}
