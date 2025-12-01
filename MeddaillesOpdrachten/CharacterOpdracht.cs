0142d6adb152850a3670243f66fcc15eafe12e7e
Marrow-SG <2242205@talnet.nl>
2025-12-01 14:23:00 +0100

    Class tasks
    These are things made during class, mainly about classes (Objects).

---
+++ MeddaillesOpdrachten/CharacterOpdracht.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeddaillesOpdracht2
+{
+    internal class CharacterOpdracht
+    {
+        private string _characterName;
+        private int _health;
+
+        public CharacterOpdracht(string characterName, int hp, bool spawned, float speed)
+        {
+            _characterName = characterName;
+            _health = hp;
+            Console.WriteLine($"A character with the name {_characterName} has been made.");
+
+        }
+
+        public void SayHello()
+        {
+            Console.WriteLine("Hey, nice to meet you.");
+        }
+
+
+        public void DamageTaken(int damageAmount) 
+        {
+            Console.WriteLine($"{_characterName} has taken {damageAmount} damage.");
+            _health -= damageAmount;
+            Console.WriteLine($"They have {_health} HP left.");
+            
+        }
+
+
+
+
+    }
+}
---
+++ MeddaillesOpdrachten/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeddaillesOpdracht2
+{
+    internal class Student
+    {
+        //Make a student class
+        //give them the following traits
+        ///Name
+        ///Age
+        ///Last Name
+        //Fill in the traits in a constructor
+        //Make a function that prints the first and last name
+        //Make two instances of students
+        //Use the function that was described earlier???
+
+        private int _studentAge;
+        private string _studentFirstName;
+        private string _studentLastName;
+
+
+        public Student(string name, string lastName, int age) 
+        {
+            _studentAge = age;
+            _studentFirstName = name;
+            _studentLastName = lastName;
+        }
+
+        public void namePrinter() 
+        {
+            Console.WriteLine($"This student's name is {_studentFirstName} {_studentLastName}.");
+
+        }
+
+
+    }
+}
